60515dca82078ee75001c65c737b4518aff1f557
meme2367 <a202205020@A202205020-01.local>
2022-05-16 13:51:13 +0900

    Refactor code style from camel case to snake case

--- practice/urls.py
+++ practice/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("functionAnnotation/1", views.functionAnnotation1),
-    path("exception/1", views.exception1),
-    path("typing/1", views.typing1),
-    path("call/1", views.call1),
-    path("inheritance/1", views.inheritance1),
-    path("decorator/1", views.decorator1),
-    path("orm/1", views.orm1),
-    path("middleware/1", views.middleware1),
-    path("log/1", views.log1),
+    path("functionAnnotation/1", views.function_annotation_1),
+    path("exception/1", views.exception_1),
+    path("typing/1", views.typing_1),
+    path("call/1", views.call_1),
+    path("inheritance/1", views.inheritance_1),
+    path("decorator/1", views.decorator_1),
+    path("orm/1", views.orm_1),
+    path("middleware/1", views.middleware_1),
+    path("log/1", views.log_1),
 ]
--- practice/views.py
+++ practice/views.py
@@ -5,54 +5,54 @@ from django.http import JsonResponse
 from typing import *
 
 
-def functionAnnotation1(request):
-    str1 = func1(1, 2, 3)
-    str2 = func2(-3)
+def function_annotation_1(request):
+    str1 = func_1(1, 2, 3)
+    str2 = func_2(-3)
     return JsonResponse(
         {"arg1": str1[0], "arg2": str1[1], "arg3": str1[2], "TrueOrFalse": str2}
     )
 
 
-def func1(arg1: str, arg2: 1 + 2, arg3: "this is annotation") -> str:
+def func_1(arg1: str, arg2: 1 + 2, arg3: "this is annotation") -> str:
     print(f"print arg1 = {arg1}")
     print(f"print arg2 = {arg2}")
     print(f"print arg3 = {arg3}")
     return arg1, arg2, arg3
 
 
-def func2(arg1: int) -> bool:
+def func_2(arg1: int) -> bool:
     if arg1 < 0:
         return False
     return True
 
 
-def exception1(request):
+def exception_1(request):
     return JsonResponse()
 
 
-def typing1(request):
+def typing_1(request):
     return JsonResponse()
 
 
-def call1(request):
+def call_1(request):
     return JsonResponse()
 
 
-def inheritance1(request):
+def inheritance_1(request):
     return JsonResponse()
 
 
-def decorator1(request):
+def decorator_1(request):
     return JsonResponse()
 
 
-def orm1(request):
+def orm_1(request):
     return JsonResponse()
 
 
-def middleware1(request):
+def middleware_1(request):
     return JsonResponse()
 
 
-def log1(request):
+def log_1(request):
     return JsonResponse()
