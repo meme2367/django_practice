0dc3c11e5d9b8930e3f497d533fc19d140ea0b03
meme2367 <a202205020@A202205020-01.local>
2022-05-16 14:57:43 +0900

    Add exception_practice.py

---
+++ practice/my_utils/exception_practice.py
@@ -0,0 +1,20 @@
+def exception_1(arg1: int) -> int:
+    try:
+        is_positive_number = func(arg1)
+        if is_positive_number == False:
+            print(f"{arg1} is negative number")
+            raise my_exception
+    except Exception as e:
+        print(e)
+    else:
+        print("예외가 발생하지 않았습니다.")
+    return arg1
+
+
+def func(arg1: int) -> bool:
+    return arg1 >= 0
+
+
+class my_exception(Exception):
+    def __init__(self):
+        super().__init__("Exception을 상속받은 내가 만든 예외입니다.")
